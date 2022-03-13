34e97b017b16a9bf9e39a042b2f354b4c9cd8dcd
AlexWills37 <alexanderwills37@gmail.com>
2022-03-13 19:15:01 -0400

    Working on animations

--- CadiKazani/Assets/Scripts/PlayAnimation.cs
+++ CadiKazani/Assets/Scripts/PlayAnimation.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Script to control an animator
+/// </summary>
 public class PlayAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("The animator to play an animation from (must have playAnim boolean parameter)")]
+    public Animator animator;
+
+    /// <summary>
+    /// Turn playAnim to true, starting the animation
+    /// </summary>
+    public void StartAnimation()
     {
-        
+        animator.SetBool("playAnim", true);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Turn playAnim to false, ending the animation
+    /// </summary>
+    public void StopAnimation()
     {
-        
+        animator.SetBool("playAnim", false);
     }
 }
