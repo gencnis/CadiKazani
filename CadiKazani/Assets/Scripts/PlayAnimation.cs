a80335efc7a5d4773df55f41b4ddf8e480546605
AlexWills37 <alexanderwills37@gmail.com>
2022-03-14 00:56:15 -0400

    All potions now move and interact

--- CadiKazani/Assets/Scripts/PlayAnimation.cs
+++ CadiKazani/Assets/Scripts/PlayAnimation.cs
@@ -10,6 +10,11 @@ public class PlayAnimation : MonoBehaviour
     [Tooltip("The animator to play an animation from (must have playAnim boolean parameter)")]
     public Animator animator;
 
+    private void Start()
+    {
+        this.animator = this.GetComponent<Animator>();
+    }
+
     /// <summary>
     /// Turn playAnim to true, starting the animation
     /// </summary>
